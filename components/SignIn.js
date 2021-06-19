250d0623dfc658fe5af367885ed45625f4417859
eduardo3g <eduardosbrasil10@gmail.com>
2021-06-19 14:50:52 -0300

    feat: create the sign in screen

---
+++ components/SignIn.js
@@ -0,0 +1,18 @@
+import { Auth } from 'aws-amplify';
+
+export default function SignIn() {
+  return (
+    <div>
+      <button
+        onClick={() => Auth.federatedSignIn({ provider: 'Google' })}
+      >
+        Sign in with Google
+      </button>
+      <button
+        onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })}
+      >
+        Sign in with Facebook
+      </button>
+    </div>
+  );
+};
--- pages/profile.js
+++ pages/profile.js
@@ -1,33 +1,69 @@
 import { useState, useEffect } from 'react';
 import { Auth } from 'aws-amplify';
 import '../configureAmplify';
+import SignIn from '../components/SignIn';
+
+const initialState = { email: '', password: '', authCode: '' };
 
 function Profile() {
+  const [uiState, setUiState] = useState(null);
+  const [formState, setFormState] = useState(initialState);
+  const [user, setUser] = useState(null);
+
   useEffect(() => {
     checkUser();
 
     async function checkUser() {
+      try {
         const user = await Auth.currentAuthenticatedUser();
-        console.log({ user });
+        setUser(user);
+        setUiState('signedIn');
+      } catch (err) {
+        setUser(null);
+        setUiState('signIn');
+      }
     }
   }, []);
 
-  return (
-    <div>
-      <button
-        onClick={() => Auth.federatedSignIn({ provider: 'Google' })}
-      >
-        Sign in with Google
-      </button>
-      <button
-        onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })}
-      >
-        Sign in with Facebook
-      </button>
+  function onChange(event) {
+    setFormState({ ...formState, [event.target.name]: e.target.value });
+  }
 
-      <button
-        onClick={() => Auth.signOut()}
-      >Sign out</button>
+  return (
+    <div
+      className="bg-gray-50 min-h-screen"
+    >
+      <div className="flex flex-col items-center">
+        <div className="max-w-full sm:w-540 mt-14">
+          <div className="bg-white py-14 px-16 shadow-form rounded">
+            {
+              uiState === 'signIn' && (
+                <SignIn
+                  onChange={onChange}
+                  setUiState={setUiState}
+                />
+              )
+            }
+            {
+              uiState === 'signedIn' && (
+                <div>
+                  <p
+                    className="text-xl"
+                  >Welcome, {user.attributes.email}.</p>
+                  <button
+                    className="text-white w-full mt-10 bg-pink-600 p-3 rounded"
+                    onClick={() => {
+                      Auth.signOut();
+                      setUiState('signIn');
+                      setUser(null);
+                    }}
+                  >Sign out</button>
+                </div>
+              )
+            }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
